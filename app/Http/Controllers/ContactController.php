445475c9519fb0c0f9a8dfee838ee00a1ddc6f0a
FarelArjun1 <farelarjun0308@gmail.com>
2022-06-15 06:41:38 +0700

    Modul 6-7

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -14,7 +14,8 @@ class ContactController extends Controller
      */
     public function index()
     {
-        return view('admin/contats/index');
+        $contact = Contact::paginate(2);
+        return view('admin/contats/index', compact('contacts'));
     }
 
     /**
@@ -39,7 +40,7 @@ class ContactController extends Controller
        $contact = Contact::create($request->all());
        $contact->save();
 
-       return redirect('contacts');
+       return redirect()->route('contacts.create');
 
     }
 
@@ -62,7 +63,8 @@ class ContactController extends Controller
      */
     public function edit($id)
     {
-        //
+        $contact = Contact::findOrFail($id);
+        return view('admin/contacts/edit', compact('contact'));
     }
 
     /**
@@ -74,7 +76,11 @@ class ContactController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $contact = Contact::findOrFail($id);
+        $contact->update($request->all());
+        $contact->save();
+
+        return redirect()->route('contacts.index');
     }
 
     /**
@@ -85,6 +91,9 @@ class ContactController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $contact = Contact::findOrFail($id);
+        $contact->delete();
+
+        return redirect()->route('contacts.index');
     }
 }
